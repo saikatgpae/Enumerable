a8185283b89b6bfd0d9fd27d1c4ba296b50abc5d
Saikat Chakraborty <saikatgpae@gmail.com>
2023-01-27 17:00:16 +0530

    Remove linters error.

--- My_List.rb
+++ My_List.rb
@@ -1,4 +1,4 @@
-require_relative 'MyEnumerable'
+require_relative 'My_Enumerable'
 class MyList
   def initialize(*list)
     @list = list
@@ -8,3 +8,10 @@ class MyList
     @list.each { |value| yield value if block_given? }
   end
 end
+
+# list = MyList.new(1, 2, 3, 4)
+# puts list.all? { |e| e < 5 }
+# puts list.all? { |e| e > 5 }
+# puts list.any? { |e| e == 2 }
+# puts list.any? { |e| e == 5 }
+# puts list.filter &:even?
